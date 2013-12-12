29b46d5750c798b68729308858e20603d19c4c53
Kamil <dev@kamil.ca>
2013-12-12 16:05:01 -0500

    Added data caching and handling of individual items

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('app', [
   'ngRoute',
-  'controllers'
+  'controllers',
+  'encoding',
 ]);
 
 app.config(['$routeProvider',
@@ -18,3 +19,38 @@ app.config(['$routeProvider',
         redirectTo: '/'
       });
   }]);
+  
+app.factory('media_library', function($http) {
+	library_data = ""
+	
+	return {
+        get_library_data: function(callback) {
+			if(library_data == "")
+			{
+				$http.get('/get_library').success(function(data)
+				{
+					callback(data)
+					library_data = data
+				});			
+			}
+			else
+			{
+				callback(library_data)
+			}
+				
+		}		
+    }
+  });
+  
+angular.module('encoding', []).
+	filter('encode_url', function() {               // filter is a factory function
+	 return function(string) { // first arg is the input, rest are filter params
+		 return btoa(string)
+	 }
+	}).
+	filter('decode_url', function() {               // filter is a factory function
+	 return function(string) { // first arg is the input, rest are filter params
+		 return atob(string)
+	 }
+	});
+	
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,19 +1,39 @@
 var controllers = angular.module('controllers', []);
  
-controllers.controller('VideoListController', ['$scope', '$http',
-  function ($scope, $http) {
+controllers.controller('VideoListController', ['$scope', '$http', 'media_library',
+  function ($scope, $http, media_library) {
       
       $scope.loading = true
-
-      $http.get('/get_library').success(function(data) {
-        $scope.libraries = data
-        $scope.loading = false
-      });
-	  
-	  
+	  media_library.get_library_data(function(data){
+          $scope.libraries = data
+          $scope.loading = false
+	  })
   }]);
  
-controllers.controller('VideoDetailController', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
+controllers.controller('VideoDetailController', ['$scope', '$routeParams', 'media_library',
+  function($scope, $routeParams, media_library) {
+	  video_id = atob($routeParams.videoId)
+	  console.log(video_id)
+	  library_data = ""
+	  
+	  media_library.get_library_data(function(data){
+          $scope.libraries = data
+		  result = null
+
+		  angular.forEach($scope.libraries, function(library, library_key){
+			  angular.forEach(library.library, function(item, item_key){
+				  if(item.path == video_id)
+				  {
+					  console.log("found")
+					  result = item
+				  }
+				  	
+			  });		 
+		  });
+		  
+		  $scope.video = result
+		  		  
+	  }) 
+
 	  
   }]);
