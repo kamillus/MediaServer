8b6385b4e048493ed3e30085760876cf23804908
Kamil <dev@kamil.ca>
2013-12-16 15:30:15 -0500

    Adding a py2exe file

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ if sys.platform == "darwin":
             ('static/js/extras', ['static/js/extras/angular-route.js']),
             ('static/partials/', ['static/partials/video-detail.html']),
             ('static/partials/', ['static/partials/video-list.html']),
+            ('static/partials/', ['static/partials/music_player.html']),
             ('templates/', ['templates/index.html']),
             ('templates/', ['templates/base.html']),
             ('lib/python2.7/', ['icon.png']),
@@ -39,5 +40,41 @@ if sys.platform == "darwin":
 
 if sys.platform == "win32":
     import py2exe
+    data = [
+            ('static\\css\\', ['static\\css\\app.css']),
+            ('static\\js\\', ['static\\js\\controllers.js']),
+            ('static\\js\\', ['static\\js\\app.js']),
+            ('static\\js\\extras', ['static\\js\\extras\\angular-route.js']),
+            ('static\\partials\\', ['static\\partials\\video-detail.html']),
+            ('static\\partials\\', ['static\\partials\\video-list.html']),
+            ('static\\partials\\', ['static\\partials\\music_player.html']),
+            ('templates\\', ['templates\\index.html']),
+            ('templates\\', ['templates\\base.html']),
+            ('lib\\python2.7\\', ['icon.png']),
+            ]
 
- 
+
+    app = ['MediaServer.py']
+    options = {
+        'excludes':['PyQt4.QtDesigner','PyQt4.QtOpenGl','PyQt4.QtScript','PyQt4.QtSql','PyQt4.QtTest','PyQt4.QtXml','PyQt4.phonon', 'PyQt4.QtHelp', 'PyQt4.QtDeclarative', 'PyQt4.QtDeclarative'],
+        "bundle_files": 3,
+        'includes': ["sip", "client", "server"]
+    }
+
+    program = [
+        {
+            "script": "MediaServer.py",
+            "icon_resources": [(1, "icon.ico")],
+            "dest_base": "Awesome Media Server",
+            "description": "Play media from your computer in the browser",
+            "author": "Awid.ca",
+        },
+    ]
+
+    setup(
+        data_files=data,
+        options={'py2exe': options},
+        windows=program,
+        name="Graboid",
+
+    )
